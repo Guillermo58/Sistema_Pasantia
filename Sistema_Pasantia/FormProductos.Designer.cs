983e4b801ad627e8ae1be29291cbfe1841b1b6e8
Guillermo58 <guillermoalbertoperezolivas@gmail.com>
2026-01-07 14:42:54 -0600

    Creacion de insertar, actualizar, vistas de el form de producto

--- Sistema_Pasantia/FormProductos.Designer.cs
+++ Sistema_Pasantia/FormProductos.Designer.cs
@@ -29,22 +29,20 @@
         private void InitializeComponent()
         {
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
-            this.button1 = new System.Windows.Forms.Button();
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.textBox2 = new System.Windows.Forms.TextBox();
-            this.textBox3 = new System.Windows.Forms.TextBox();
-            this.textBox4 = new System.Windows.Forms.TextBox();
-            this.textBox5 = new System.Windows.Forms.TextBox();
-            this.textBox6 = new System.Windows.Forms.TextBox();
-            this.button2 = new System.Windows.Forms.Button();
-            this.button3 = new System.Windows.Forms.Button();
-            this.button4 = new System.Windows.Forms.Button();
-            this.label1 = new System.Windows.Forms.Label();
+            this.Eliminar = new System.Windows.Forms.Button();
+            this.txtCodigo = new System.Windows.Forms.TextBox();
+            this.txtNombre = new System.Windows.Forms.TextBox();
+            this.txtExistencia = new System.Windows.Forms.TextBox();
+            this.txtEstado = new System.Windows.Forms.TextBox();
+            this.txtProveedor = new System.Windows.Forms.TextBox();
+            this.Actualizar = new System.Windows.Forms.Button();
+            this.Opciones = new System.Windows.Forms.Button();
             this.label2 = new System.Windows.Forms.Label();
             this.label4 = new System.Windows.Forms.Label();
             this.label5 = new System.Windows.Forms.Label();
             this.label6 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
+            this.Insertar = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
@@ -56,97 +54,75 @@
             this.dataGridView1.Size = new System.Drawing.Size(524, 146);
             this.dataGridView1.TabIndex = 0;
             // 
-            // button1
+            // Eliminar
             // 
-            this.button1.Location = new System.Drawing.Point(577, 371);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 4;
-            this.button1.Text = "Eliminar";
-            this.button1.UseVisualStyleBackColor = true;
+            this.Eliminar.Location = new System.Drawing.Point(577, 371);
+            this.Eliminar.Name = "Eliminar";
+            this.Eliminar.Size = new System.Drawing.Size(75, 23);
+            this.Eliminar.TabIndex = 4;
+            this.Eliminar.Text = "Eliminar";
+            this.Eliminar.UseVisualStyleBackColor = true;
+            this.Eliminar.Click += new System.EventHandler(this.Eliminar_Click);
             // 
-            // textBox1
+            // txtCodigo
             // 
-            this.textBox1.Location = new System.Drawing.Point(168, 248);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(100, 20);
-            this.textBox1.TabIndex = 5;
+            this.txtCodigo.Location = new System.Drawing.Point(180, 274);
+            this.txtCodigo.Name = "txtCodigo";
+            this.txtCodigo.Size = new System.Drawing.Size(100, 20);
+            this.txtCodigo.TabIndex = 6;
             // 
-            // textBox2
+            // txtNombre
             // 
-            this.textBox2.Location = new System.Drawing.Point(168, 293);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(100, 20);
-            this.textBox2.TabIndex = 6;
+            this.txtNombre.Location = new System.Drawing.Point(378, 248);
+            this.txtNombre.Name = "txtNombre";
+            this.txtNombre.Size = new System.Drawing.Size(100, 20);
+            this.txtNombre.TabIndex = 7;
             // 
-            // textBox3
+            // txtExistencia
             // 
-            this.textBox3.Location = new System.Drawing.Point(378, 248);
-            this.textBox3.Name = "textBox3";
-            this.textBox3.Size = new System.Drawing.Size(100, 20);
-            this.textBox3.TabIndex = 7;
+            this.txtExistencia.Location = new System.Drawing.Point(378, 293);
+            this.txtExistencia.Name = "txtExistencia";
+            this.txtExistencia.Size = new System.Drawing.Size(100, 20);
+            this.txtExistencia.TabIndex = 8;
             // 
-            // textBox4
+            // txtEstado
             // 
-            this.textBox4.Location = new System.Drawing.Point(378, 293);
-            this.textBox4.Name = "textBox4";
-            this.textBox4.Size = new System.Drawing.Size(100, 20);
-            this.textBox4.TabIndex = 8;
+            this.txtEstado.Location = new System.Drawing.Point(578, 250);
+            this.txtEstado.Name = "txtEstado";
+            this.txtEstado.Size = new System.Drawing.Size(100, 20);
+            this.txtEstado.TabIndex = 9;
             // 
-            // textBox5
+            // txtProveedor
             // 
-            this.textBox5.Location = new System.Drawing.Point(578, 250);
-            this.textBox5.Name = "textBox5";
-            this.textBox5.Size = new System.Drawing.Size(100, 20);
-            this.textBox5.TabIndex = 9;
+            this.txtProveedor.Location = new System.Drawing.Point(578, 293);
+            this.txtProveedor.Name = "txtProveedor";
+            this.txtProveedor.Size = new System.Drawing.Size(100, 20);
+            this.txtProveedor.TabIndex = 10;
             // 
-            // textBox6
+            // Actualizar
             // 
-            this.textBox6.Location = new System.Drawing.Point(578, 293);
-            this.textBox6.Name = "textBox6";
-            this.textBox6.Size = new System.Drawing.Size(100, 20);
-            this.textBox6.TabIndex = 10;
+            this.Actualizar.Location = new System.Drawing.Point(445, 371);
+            this.Actualizar.Name = "Actualizar";
+            this.Actualizar.Size = new System.Drawing.Size(75, 23);
+            this.Actualizar.TabIndex = 11;
+            this.Actualizar.Text = "Actualizar";
+            this.Actualizar.UseVisualStyleBackColor = true;
+            this.Actualizar.Click += new System.EventHandler(this.Actualizar_Click);
             // 
-            // button2
+            // Opciones
             // 
-            this.button2.Location = new System.Drawing.Point(445, 371);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(75, 23);
-            this.button2.TabIndex = 11;
-            this.button2.Text = "Actualizar";
-            this.button2.UseVisualStyleBackColor = true;
-            // 
-            // button3
-            // 
-            this.button3.Location = new System.Drawing.Point(287, 371);
-            this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(108, 23);
-            this.button3.TabIndex = 12;
-            this.button3.Text = "Lista Opciones";
-            this.button3.UseVisualStyleBackColor = true;
-            // 
-            // button4
-            // 
-            this.button4.Location = new System.Drawing.Point(163, 371);
-            this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(75, 23);
-            this.button4.TabIndex = 13;
-            this.button4.Text = "Insertar";
-            this.button4.UseVisualStyleBackColor = true;
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(95, 254);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(67, 13);
-            this.label1.TabIndex = 14;
-            this.label1.Text = "ID_Producto";
+            this.Opciones.Location = new System.Drawing.Point(287, 371);
+            this.Opciones.Name = "Opciones";
+            this.Opciones.Size = new System.Drawing.Size(108, 23);
+            this.Opciones.TabIndex = 12;
+            this.Opciones.Text = "Lista Opciones";
+            this.Opciones.UseVisualStyleBackColor = true;
+            this.Opciones.Click += new System.EventHandler(this.Opciones_Click);
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(122, 300);
+            this.label2.Location = new System.Drawing.Point(134, 281);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(40, 13);
             this.label2.TabIndex = 15;
@@ -188,27 +164,35 @@
             this.label3.TabIndex = 20;
             this.label3.Text = "Nombre";
             // 
+            // Insertar
+            // 
+            this.Insertar.Location = new System.Drawing.Point(168, 370);
+            this.Insertar.Name = "Insertar";
+            this.Insertar.Size = new System.Drawing.Size(75, 23);
+            this.Insertar.TabIndex = 21;
+            this.Insertar.Text = "Insertar";
+            this.Insertar.UseVisualStyleBackColor = true;
+            this.Insertar.Click += new System.EventHandler(this.Insertar_Click);
+            // 
             // FormProductos
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(854, 489);
+            this.Controls.Add(this.Insertar);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label6);
             this.Controls.Add(this.label5);
             this.Controls.Add(this.label4);
             this.Controls.Add(this.label2);
-            this.Controls.Add(this.label1);
-            this.Controls.Add(this.button4);
-            this.Controls.Add(this.button3);
-            this.Controls.Add(this.button2);
-            this.Controls.Add(this.textBox6);
-            this.Controls.Add(this.textBox5);
-            this.Controls.Add(this.textBox4);
-            this.Controls.Add(this.textBox3);
-            this.Controls.Add(this.textBox2);
-            this.Controls.Add(this.textBox1);
-            this.Controls.Add(this.button1);
+            this.Controls.Add(this.Opciones);
+            this.Controls.Add(this.Actualizar);
+            this.Controls.Add(this.txtProveedor);
+            this.Controls.Add(this.txtEstado);
+            this.Controls.Add(this.txtExistencia);
+            this.Controls.Add(this.txtNombre);
+            this.Controls.Add(this.txtCodigo);
+            this.Controls.Add(this.Eliminar);
             this.Controls.Add(this.dataGridView1);
             this.Name = "FormProductos";
             this.Text = "FormProductos";
@@ -221,21 +205,19 @@
         #endregion
 
         private System.Windows.Forms.DataGridView dataGridView1;
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.TextBox textBox2;
-        private System.Windows.Forms.TextBox textBox3;
-        private System.Windows.Forms.TextBox textBox4;
-        private System.Windows.Forms.TextBox textBox5;
-        private System.Windows.Forms.TextBox textBox6;
-        private System.Windows.Forms.Button button2;
-        private System.Windows.Forms.Button button3;
-        private System.Windows.Forms.Button button4;
-        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button Eliminar;
+        private System.Windows.Forms.TextBox txtCodigo;
+        private System.Windows.Forms.TextBox txtNombre;
+        private System.Windows.Forms.TextBox txtExistencia;
+        private System.Windows.Forms.TextBox txtEstado;
+        private System.Windows.Forms.TextBox txtProveedor;
+        private System.Windows.Forms.Button Actualizar;
+        private System.Windows.Forms.Button Opciones;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label4;
         private System.Windows.Forms.Label label5;
         private System.Windows.Forms.Label label6;
         private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button Insertar;
     }
 }
--- Sistema_Pasantia/FormProductos.cs
+++ Sistema_Pasantia/FormProductos.cs
@@ -6,15 +6,146 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Data.SqlClient;
 using System.Windows.Forms;
 
 namespace Sistema_Pasantia
 {
     public partial class FormProductos : Form
     {
+        SqlConnection conexion = new SqlConnection(
+            @"Data Source=MSI\SQLEXPRESS;Initial Catalog=Practica_Pasante;Integrated Security=True;Encrypt=True;TrustServerCertificate=True"
+        );
+
+        int idProductoSeleccionado = 0;
+
         public FormProductos()
         {
             InitializeComponent();
+        }
+
+        private void FormProductos_Load(object sender, EventArgs e)
+        {
+            CargarProductos();
+        }
+
+        void CargarProductos()
+        {
+            try
+            {
+                SqlDataAdapter da = new SqlDataAdapter(
+                    "SELECT ID_Producto, Codigo, Nombre, Existencia, Estado, Proveedor FROM Producto",
+                    conexion
+                );
+
+                DataTable dt = new DataTable();
+                da.Fill(dt);
+                dataGridView1.DataSource = dt;
+
+                dataGridView1.Columns["ID_Producto"].Visible = false;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error al cargar productos: " + ex.Message);
+            }
+        }
+
+
+        private void Insertar_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string sql = "INSERT INTO Producto (Codigo, Nombre, Existencia, Estado, Proveedor) " +
+                             "VALUES (@Codigo, @Nombre, @Existencia, @Estado, @Proveedor)";
+
+                SqlCommand cmd = new SqlCommand(sql, conexion);
+                cmd.Parameters.AddWithValue("@Codigo", txtCodigo.Text);
+                cmd.Parameters.AddWithValue("@Nombre", txtNombre.Text);
+                cmd.Parameters.AddWithValue("@Existencia", txtExistencia.Text);
+                cmd.Parameters.AddWithValue("@Estado", txtEstado.Text);
+                cmd.Parameters.AddWithValue("@Proveedor", txtProveedor.Text);
+
+                conexion.Open();
+                cmd.ExecuteNonQuery();
+                conexion.Close();
+
+                CargarProductos();
+                LimpiarCampos();
+
+                MessageBox.Show("Producto insertado correctamente");
+            }
+            catch (Exception ex)
+            {
+                conexion.Close();
+                MessageBox.Show("Error al insertar: " + ex.Message);
+            }
+        }
+
+
+        private void Opciones_Click(object sender, EventArgs e)
+        {
+            CargarProductos();
+        }
+
+        private void dataGridView1_SelectionChanged(object sender, EventArgs e)
+        {
+            if (dataGridView1.SelectedRows.Count > 0)
+            {
+                DataGridViewRow fila = dataGridView1.SelectedRows[0];
+
+                idProductoSeleccionado = Convert.ToInt32(fila.Cells["ID_Producto"].Value);
+
+                txtCodigo.Text = fila.Cells["Codigo"].Value.ToString();
+                txtNombre.Text = fila.Cells["Nombre"].Value.ToString();
+                txtExistencia.Text = fila.Cells["Existencia"].Value.ToString();
+                txtEstado.Text = fila.Cells["Estado"].Value.ToString();
+                txtProveedor.Text = fila.Cells["Proveedor"].Value.ToString();
+            }
+        }
+
+
+        private void Actualizar_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string sql = "UPDATE Producto SET Nombre=@Nombre, " +
+                             "Existencia=@Existencia, Estado=@Estado, Proveedor=@Proveedor " +
+                             "WHERE Codigo=@Codigo";
+
+                SqlCommand cmd = new SqlCommand(sql, conexion);
+                cmd.Parameters.AddWithValue("@Codigo", txtCodigo.Text);
+                cmd.Parameters.AddWithValue("@Nombre", txtNombre.Text);
+                cmd.Parameters.AddWithValue("@Existencia", txtExistencia.Text);
+                cmd.Parameters.AddWithValue("@Estado", txtEstado.Text);
+                cmd.Parameters.AddWithValue("@Proveedor", txtProveedor.Text);
+      
+
+                conexion.Open();
+                cmd.ExecuteNonQuery();
+                conexion.Close();
+
+                CargarProductos();
+                LimpiarCampos();
+
+                MessageBox.Show("Producto actualizado correctamente");
+            }
+            catch (Exception ex)
+            {
+                conexion.Close();
+                MessageBox.Show("Error al actualizar: " + ex.Message);
+            }
+        }
+
+        
+
+        void LimpiarCampos()
+        {
+            idProductoSeleccionado = 0;
+            txtCodigo.Clear();
+            txtNombre.Clear();
+            txtExistencia.Clear();
+            txtEstado.Clear();
+            txtProveedor.Clear();
         }
 
     }
